fb23f0ded6306dce7d323f854085a7a98ba834e4
momchiltues19 <mom.todorov.tues19@gmail.com>
2021-11-04 20:12:00 +0200

    babba

--- bask3.cpp
+++ bask3.cpp
@@ -4,6 +4,47 @@ using namespace std;
 
 int main()
 {
+	int num;
+	cout << "Input a number: ";
+	cin >> num;
+
+	for (int perfect = 1; perfect < num; perfect++)
+	{
+		
+		int sum = 0;
+		for (int j = 1; j < perfect; j++)
+		{
+			if (perfect % j == 0)
+			{
+				sum += j;
+			}
+		}
+		if (sum == perfect)
+		{
+			cout << perfect << " - ";
+			for (int prime = 2; prime <= perfect; prime++)
+			{
+				if (perfect % prime == 0)
+				{
+					bool isPrime = true;
+					for (int h = 2; h < prime && isPrime; h++)
+					{
+						if (prime % h == 0)
+						{
+							isPrime = false;
+						}
+
+					}
+					if (isPrime)
+					{
+						cout << prime << ", ";
+					}
+				}
+			}
+			cout << endl;
+		}
+	}
+
 	return 0;
 }
 
